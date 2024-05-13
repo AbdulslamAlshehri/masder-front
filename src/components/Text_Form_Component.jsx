4604a9f2235f36b0d85708c448905e3e3c1ad8ba
Abdulslam <120113778+AbdulslamAlshehri@users.noreply.github.com>
2024-05-14 02:44:20 +0300

    Update Text_Form_Component.jsx

--- src/components/Text_Form_Component.jsx
+++ src/components/Text_Form_Component.jsx
@@ -48,27 +48,26 @@ const Text_Form_Component = () => {
     }
   };
 
-  const Handle_Form_Submit = async (Event) => {
-    Event.preventDefault();
-    console.log('Show Text submitted:', text);
-    
-      // const response = await 
-      axios.post('https://masdar2-production.up.railway.app/ai/text', { text })
-  .then(response => {
-      console.log('Text uploaded successfully', response.data);
-      console.log('Text uploaded successfully', response.data);
-      set_Upload_Status('Text uploaded successfully');
-      var percentage = response.data.score * 100;
-      percentage = percentage.toFixed(2);
-      set_Percentage(percentage)
-      setContentType("Text");
-      Submit_Form()};
-  })
-  .catch(error => {
-      console.error('Error uploading text', error);
-      set_Upload_Status('Error uploading text');
-  });
-  };
+const Handle_Form_Submit = async (Event) => {
+  Event.preventDefault();
+  console.log('Show Text submitted:', text);
+  
+  axios.post('https://masdar2-production.up.railway.app/ai/text', { text })
+    .then(response => {
+        console.log('Text uploaded successfully', response.data);
+        set_Upload_Status('Text uploaded successfully');
+        var percentage = response.data.score * 100;
+        percentage = percentage.toFixed(2);
+        set_Percentage(percentage);
+        setContentType("Text");
+        Submit_Form(); // Ensure this call is within the `then` block
+    })
+    .catch(error => {
+        console.error('Error uploading text', error);
+        set_Upload_Status('Error uploading text');
+    });
+};
+
 
   return (
     <>
