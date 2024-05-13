067b8807ce95ee8a6107497aa51774f3eb5ef201
Abdulslam <120113778+AbdulslamAlshehri@users.noreply.github.com>
2024-05-14 00:14:29 +0300

    Update Header2.js

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -66,7 +66,7 @@ import axios from "axios";
       
 
      <svg className='icon'xmlns="http://www.w3.org/2000/svg" height="34" width="29" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg><span className='Pos1'>
-     <NavDropdown
+     <NavDropdown style={{color:"black"}}
               id="nav-dropdown-dark-example"
               title= {title}                              //{title} // Use the title from the state
               menuVariant="dark"
