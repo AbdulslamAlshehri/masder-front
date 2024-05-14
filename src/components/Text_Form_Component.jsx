130bcd706477e5b08c43408390b238a0ba43bd54
Abdulslam <120113778+AbdulslamAlshehri@users.noreply.github.com>
2024-05-14 03:00:55 +0300

    Update Text_Form_Component.jsx

--- src/components/Text_Form_Component.jsx
+++ src/components/Text_Form_Component.jsx
@@ -125,7 +125,6 @@ const Handle_Form_Submit = async (Event) => {
             <Row>
               <Col style={{ textAlign: "center" }}><h5 style={{fontSize:"30px"}}>{percentage}%</h5></Col>
               <Col style={{ textAlign: "center" }}><h5 style={{fontSize:"30px"}}>{contentType}</h5></Col>
-              <Col style={{ textAlign: "center" }}><h5 style={{fontSize:"30px"}}>{SubmitHistory}</h5></Col>
             </Row>
             <Row>
               <Col style={{ textAlign: "center" }}>AI</Col>
